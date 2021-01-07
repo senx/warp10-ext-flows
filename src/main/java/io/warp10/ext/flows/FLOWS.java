072eb3147d99a5ca4e5e5843ce090d4b36b70006
Mathias Herberts <Mathias.Herberts+git@gmail.com>
2021-01-07 09:54:01 +0100

    Added check to ensure the whole input was consumed

--- src/main/java/io/warp10/ext/flows/FLOWS.java
+++ src/main/java/io/warp10/ext/flows/FLOWS.java
@@ -1,5 +1,5 @@
 //
-// Copyright 2020  SenX S.A.S.
+// Copyright 2020-2021  SenX S.A.S.
 //
 // Use of this software is governed by the Business Source License
 // included in the file licenses/BSL.txt.
@@ -23,13 +23,16 @@ import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.Parser;
 import org.antlr.v4.runtime.RecognitionException;
 import org.antlr.v4.runtime.Recognizer;
+import org.antlr.v4.runtime.Token;
 import org.antlr.v4.runtime.atn.ATNConfigSet;
 import org.antlr.v4.runtime.dfa.DFA;
+import org.apache.commons.lang3.StringUtils;
 
 import io.warp10.ext.flows.antlr.FLoWSExecutor;
 import io.warp10.ext.flows.antlr.FLoWSLexer;
 import io.warp10.ext.flows.antlr.FLoWSParser;
 import io.warp10.ext.flows.antlr.FLoWSParser.BlockStatementsContext;
+import io.warp10.quasar.token.thrift.data.TokenType;
 import io.warp10.script.MemoryWarpScriptStack;
 import io.warp10.script.NamedWarpScriptFunction;
 import io.warp10.script.WarpScriptException;
@@ -37,33 +40,33 @@ import io.warp10.script.WarpScriptStack;
 import io.warp10.script.WarpScriptStackFunction;
 
 public class FLOWS extends NamedWarpScriptFunction implements WarpScriptStackFunction {
-  
+
   private final boolean eval;
-  
+
   private static final long[] SIPHASH = { 0x42L, 0x123L };
-  
+
   public FLOWS(String name, boolean eval) {
     super(name);
     this.eval = eval;
   }
-  
+
   @Override
   public Object apply(WarpScriptStack stack) throws WarpScriptException {
     Object top = stack.pop();
-    
+
     if (!(top instanceof String)) {
       throw new WarpScriptException(getName() + " operates on a STRING.");
     }
-    
+
     String code = (String) top;
-    
+
     //
     // Parse and generate
     //
-    
+
     try {
       byte[] bytes = code.getBytes(StandardCharsets.UTF_8);
-      
+
       ANTLRErrorListener listener = new ANTLRErrorListener() {
         @Override
         public void reportAmbiguity(Parser recognizer, DFA dfa, int startIndex, int stopIndex, boolean exact, BitSet ambigAlts, ATNConfigSet configs) {
@@ -79,7 +82,7 @@ public class FLOWS extends NamedWarpScriptFunction implements WarpScriptStackFun
           throw new RuntimeException("Syntax Error line " + line + ":" + charPositionInLine + ", " + msg);
         }
       };
-      
+
       ByteArrayInputStream in = new ByteArrayInputStream(bytes);
       CharStream input = CharStreams.fromStream(in);
       FLoWSLexer lexer = new FLoWSLexer(input);
@@ -88,7 +91,15 @@ public class FLOWS extends NamedWarpScriptFunction implements WarpScriptStackFun
       FLoWSParser parser = new FLoWSParser(tokens);
       parser.addErrorListener(listener);
       BlockStatementsContext context = parser.blockStatements();
-      
+
+      //
+      // Check if we consumed the whole input, if not emit an errot.
+      //
+
+      if (Token.EOF != parser.getCurrentToken().getType()) {
+        throw new WarpScriptException(getName() + " found extraneous content on line L" + parser.getCurrentToken().getLine() + ":" + parser.getCurrentToken().getCharPositionInLine() + " at '" + StringUtils.abbreviateMiddle(parser.getCurrentToken().getText(), "...", 32) + "'.");
+      }
+
       FLoWSExecutor generator = new FLoWSExecutor(eval ? (MemoryWarpScriptStack) stack : null);
       generator.indent();
       generator.generate(context);
@@ -100,7 +111,7 @@ public class FLOWS extends NamedWarpScriptFunction implements WarpScriptStackFun
     } catch (Throwable t) {
       throw new WarpScriptException(getName() + " encountered an error.", t);
     }
-    
+
     return stack;
   }
 }
